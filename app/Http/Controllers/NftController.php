87086355db137d0a454c737f6b5001bdac5f460c
Damilola Olowookere <damms005@gmail.com>
2022-08-23 17:19:15 +0100

    First commit

---
+++ app/Http/Controllers/NftController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNftRequest;
+use App\Http\Requests\UpdateNftRequest;
+use App\Models\Nft;
+
+class NftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNftRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreNftRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nft  $nft
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nft $nft)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nft  $nft
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nft $nft)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNftRequest  $request
+     * @param  \App\Models\Nft  $nft
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNftRequest $request, Nft $nft)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nft  $nft
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nft $nft)
+    {
+        //
+    }
+}
